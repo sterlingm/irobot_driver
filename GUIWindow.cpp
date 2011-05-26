1d9d9c61387715e969a30a5cda0d40f6c992f99a
Sterling <sterling@sterling-laptop.(none)>
2011-05-26 17:12:04 -0400

    Adding GUIWindow files

---
+++ GUIWindow.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include "GUIWindow.h"
+using namespace std;
+
+//all code in here should be self explanatory
+
+
+GUIWindow::GUIWindow(Robot& r) : Fl_Window(500, 350, "iRobot Create"), robot(r) {
+    begin();    //make the widgets, button for each option
+        drive = new Fl_Button(50, 75, 70, 30, "Drive");
+        drive->callback(cb_drive, this);
+        drive->shortcut('d');
+
+        turnCW = new Fl_Button(280, 75, 120, 30, "Turn Clockwise");
+        turnCW->callback(cb_turnCW, this);
+        turnCW->shortcut('c');
+
+        turnCCW = new Fl_Button(50, 175, 180, 30, "Turn CounterClockwise");
+        turnCCW->callback(cb_turnCCW, this);
+        turnCCW->shortcut(FL_CTRL + 'c');
+
+        stop = new Fl_Button(350, 175, 50, 30, "Stop");
+        stop->callback(cb_stop, this);
+        stop->shortcut('s');
+
+        quit = new Fl_Button(350, 275, 50, 30, "Quit");
+        quit->callback(cb_quit, this);
+        quit->shortcut('q');
+
+        fullMode = new Fl_Button(50, 275, 80, 30, "Full Mode");
+        fullMode->callback(cb_fullmode, this);
+        fullMode->shortcut('f');
+
+        safeMode = new Fl_Button(150, 275, 80, 30, "Safe Mode");
+        safeMode->callback(cb_safemode, this);
+        safeMode->shortcut('s');
+    end();  //end making widgets
+    resizable(this);
+    show(); //show it
+}
+
+
+//same as before, just able to specify dimensions
+GUIWindow::GUIWindow(int w, int h, const char* title, Robot& r) : Fl_Window(w, h, title), robot(r) {
+    begin();
+        drive = new Fl_Button(50, 75, 70, 30, "Drive");
+        drive->callback(cb_drive, this);
+        drive->shortcut('d');
+
+        turnCW = new Fl_Button(280, 75, 120, 30, "Turn Clockwise");
+        turnCW->callback(cb_turnCW, this);
+        turnCW->shortcut('c');
+
+        turnCCW = new Fl_Button(50, 175, 180, 30, "Turn CounterClockwise");
+        turnCCW->callback(cb_turnCCW, this);
+        turnCCW->shortcut(FL_CTRL + 'c');
+
+        stop = new Fl_Button(310, 175, 50, 30, "Stop");
+        stop->callback(cb_stop, this);
+        stop->shortcut('s');
+
+        quit = new Fl_Button(150, 275, 50, 30, "Quit");
+        quit->callback(cb_quit, this);
+        quit->shortcut('q');
+
+        fullMode = new Fl_Button(50, 275, 80, 30, "Full Mode");
+        fullMode->callback(cb_fullmode, this);
+        fullMode->shortcut('f');
+
+        safeMode = new Fl_Button(150, 275, 80, 30, "Safe Mode");
+        safeMode->callback(cb_safemode, this);
+        safeMode->shortcut('s');
+    end();
+    resizable(this);
+    show();
+}
+
+GUIWindow::~GUIWindow() {}
+
+
+
+
+//CALLBACK FUNCTIONS
+//each just calls the robot's respective function
+
+
+void GUIWindow::cb_drive(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_drive_i();
+}
+
+void GUIWindow::cb_drive_i() {
+    robot.drive_straight(100);
+}
+
+void GUIWindow::cb_turnCW(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_turnCW_i();
+}
+
+void GUIWindow::cb_turnCW_i() {
+    robot.turnClockwise(100);
+}
+
+void GUIWindow::cb_turnCCW(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_turnCCW_i();
+}
+
+void GUIWindow::cb_turnCCW_i() {
+    robot.turnCounterClockwise(100);
+}
+
+void GUIWindow::cb_stop(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_stop_i();
+}
+
+void GUIWindow::cb_stop_i() {
+    robot.stop();
+}
+
+
+void GUIWindow::cb_fullmode(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_fullmode_i();
+}
+
+void GUIWindow::cb_fullmode_i() {
+    robot.fullMode();
+}
+
+void GUIWindow::cb_safemode(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_safemode_i();
+}
+
+void GUIWindow::cb_safemode_i() {
+    robot.safeMode();
+}
+
+
+void GUIWindow::cb_quit(Fl_Widget* o, void* v) {
+    GUIWindow* w = (GUIWindow*)v;
+    w->cb_quit_i();
+}
+
+void GUIWindow::cb_quit_i() {
+    robot.stop();
+    hide();
+}
+
+/*
+int main() {
+    Robot robot(16, 57600);
+    GUIWindow w(robot);
+    return Fl::run();
+}
+*/
---
+++ GUIWindow.h
@@ -0,0 +1,47 @@
+
+#ifndef GUIWINDOW_H
+#define GUIWINDOW_H
+#include "Robot.h"
+#include</home/sterling/Downloads/fltk-1.3.x-r8695/FL/Fl.H>
+#include </home/sterling/Downloads/fltk-1.3.x-r8695/FL/Fl_Window.H>
+#include </home/sterling/Downloads/fltk-1.3.x-r8695/FL/Fl_Button.H>
+
+class GUIWindow : public Fl_Window{
+public:
+    GUIWindow(Robot&);
+    GUIWindow(int, int, const char*, Robot&);
+    ~GUIWindow();
+
+    Fl_Button* drive;
+    Fl_Button* turnCW;
+    Fl_Button* turnCCW;
+    Fl_Button* stop;
+    Fl_Button* quit;
+    Fl_Button* fullMode;
+    Fl_Button* safeMode;
+
+private:
+    Robot robot;
+
+    static void cb_drive(Fl_Widget*, void*);
+    inline void cb_drive_i();
+
+    static void cb_turnCW(Fl_Widget*, void*);
+    inline void cb_turnCW_i();
+
+    static void cb_turnCCW(Fl_Widget*, void*);
+    inline void cb_turnCCW_i();
+
+    static void cb_stop(Fl_Widget*, void*);
+    inline void cb_stop_i();
+
+    static void cb_quit(Fl_Widget*, void*);
+    inline void cb_quit_i();
+
+    static void cb_fullmode(Fl_Widget*, void*);
+    inline void cb_fullmode_i();
+
+    static void cb_safemode(Fl_Widget*, void*);
+    inline void cb_safemode_i();
+};
+#endif
